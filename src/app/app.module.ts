7c7802e6c1c2aaa42525dec627ae9114b0913889
mwikalikyalo <winniemwikali07@gmail.com>
2022-10-24 15:22:18 +0300

    Add the app's initial structure and the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
