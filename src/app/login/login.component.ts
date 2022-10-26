afe54c6acac666faf88716bf3a7d0093698276fe
mwikalikyalo <winniemwikali07@gmail.com>
2022-10-26 10:08:06 +0300

    Add the shopperz icon

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,11 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  faEnvelope= faEnvelope;
+  faEnvelope = faEnvelope;
   faFacebook = faFacebook;
-  faSquareTwitter=faSquareTwitter;
+  faSquareTwitter = faSquareTwitter;
+  faCartShopping = faCartShopping;
+  
   constructor() { }
 
   ngOnInit(): void {
